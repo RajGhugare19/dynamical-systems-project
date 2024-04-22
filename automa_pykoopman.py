b1200cd84cf56e2f5fc0262fdb24880367af66e7
RajGhugare19 <rajghugare.vnit@gmail.com>
2024-04-21 20:09:08 -0400

    adding code

--- automa_pykoopman.py
+++ automa_pykoopman.py
@@ -13,11 +13,11 @@ import itertools
 import cellpylib as cpl
 import numpy.random as rnd
 from sklearn.metrics import mean_squared_error
-np.random.seed(42)  # for reproducibility
 import warnings
 warnings.filterwarnings('ignore')
 from automata import gather_rule_trajs_from_starts
 import pickle as pkl
+import json
 
 def num_unique_local_operations(trajs):
     neighbourhoods = list(itertools.product([0.0, 1.0], repeat=3)) # 3 bcs we consider neighbourhoods of size 3
@@ -60,12 +60,53 @@ def slice_array_at_indices(arr, indices, filter_trajs_len=1):
     
     return final_list
             
+def generate_data_for_strong_comp_raj(state_size, rule, results_dir, seed):
+    # shuffle with a fixed seed
+    start_state_permutations = list(itertools.product([0.0, 1.0], repeat=state_size))
+    
+    random.Random(seed).shuffle(start_state_permutations)
 
-def generate_data(state_size, traj_len, rule, compo_test_type, results_dir):
+    train_start_state_permutations_1_3 = list(itertools.product([0.0, 1.0], repeat=3))[:4]
+    val_start_state_permutations_1_3 = list(itertools.product([0.0, 1.0], repeat=3))[4:6]
+    test_start_state_permutations_1_3 = list(itertools.product([0.0, 1.0], repeat=3))[6:]
+
+    train_perms = []
+    val_perms = []
+    test_perms = []
+
+    for sss in start_state_permutations:
+        if sss[:3] in train_start_state_permutations_1_3:
+            train_perms.append(sss)
+        elif sss[:3] in val_start_state_permutations_1_3:
+            val_perms.append(sss)
+        else:
+            assert sss[:3] in test_start_state_permutations_1_3
+            test_perms.append(sss)
+     
+    print("Num train trajs: ", len(train_perms))
+    print("Num val trajs: ", len(val_perms))
+    print("Num test trajs: ", len(test_perms))
+
+    train_trajs = gather_rule_trajs_from_starts(starts=train_perms, traj_len=2, rule=rule)
+    val_trajs = gather_rule_trajs_from_starts(starts=val_perms, traj_len=2, rule=rule)
+    test_trajs = gather_rule_trajs_from_starts(starts=test_perms, traj_len=2, rule=rule)
+    
+    local_to_count = num_unique_local_operations(train_trajs).values()
+    
+    # check for local operation coverage
+    if(0 in local_to_count):
+        raise NotImplementedError("We did not implement a way to deal with a training set that does not cover all local evolution operations. This is the case here.\
+                                  Pick another automata rule to generate a different dataset hopefully without this problem. That should be the case for all rules defined in cellpylib.")
+    else:
+        print("Check for local evolution operation coverage succeeded for rule {}!".format(str(rule)))
+
+    return train_trajs, val_trajs, test_trajs
+
+def generate_data(state_size, traj_len, rule, compo_test_type, results_dir, seed):
 
     # shuffle with a fixed seed
     start_state_permutations = list(itertools.product([0.0, 1.0], repeat=state_size))
-    random.Random(0).shuffle(start_state_permutations) # this is fine because our state space is usually small enough
+    random.Random(seed).shuffle(start_state_permutations) # this is fine because our state space is usually small enough
     end_train = int(len(start_state_permutations)*0.65) 
     end_val = end_train + int(len(start_state_permutations)*0.20)
     
@@ -77,14 +118,7 @@ def generate_data(state_size, traj_len, rule, compo_test_type, results_dir):
     print("Num val trajs: ", len(val_perms))
     print("Num test trajs: ", len(test_perms))
     
-    
-    # print("train starts: ", train_perms)
-    # print("val starts: ", val_perms)
-    # print("test starts: ", test_perms)
-    
-    train_trajs = gather_rule_trajs_from_starts(starts=train_perms, traj_len=traj_len, rule=rule)
-    # val_states = [np.array(tup) for tup in val_perms]
-    # test_states = [np.array(tup) for tup in test_perms]
+    train_trajs = gather_rule_trajs_from_starts(starts=train_perms, traj_len=2, rule=rule)
     val_trajs = gather_rule_trajs_from_starts(starts=val_perms, traj_len=2, rule=rule)
     test_trajs = gather_rule_trajs_from_starts(starts=test_perms, traj_len=2, rule=rule)
     
@@ -98,82 +132,40 @@ def generate_data(state_size, traj_len, rule, compo_test_type, results_dir):
     else:
         print("Check for local evolution operation coverage succeeded for rule {}!".format(str(rule)))
     
-    # TODO slice train trajectories so that start states in test and val do not occur in train.
-    # pykoopman can deal with ariable length trajectories, so this is fine
-    
-    print("Running compositionality-aware dataset pre-processing...")
-    val_test_checking_set = set(val_perms).union(set(test_perms))
-    
-    
-    if(compo_test_type == "weak"):
-        final_train_trajs = []
-        
-        # go through every train traj
-        for tr_idx in tqdm(range(len(train_trajs))):
-            tr = train_trajs[tr_idx]
-            traj_slice_indices = []
-            # in traj, go through every state
-            for s_idx in range(len(tr)):  
-                # check if state it occurs in test or val start states
-                if(tuple(tr[s_idx]) in val_test_checking_set):
-                    # if yes, store index to later slice train traj into two sections: before state and after state.
-                    traj_slice_indices.append(s_idx)
-            
-                    
-            final_train_trajs.extend(slice_array_at_indices(tr, traj_slice_indices))
-            
-        
-        with open("{}/filtered_train_trajs.pkl".format(results_dir), 'wb') as outfile:
-            pkl.dump(final_train_trajs, outfile)
-        
-        with open("{}/val_states.pkl".format(results_dir), 'wb') as outfile:
-            pkl.dump(val_trajs, outfile)
-        
-        with open("{}/test_states.pkl".format(results_dir), 'wb') as outfile:
-            pkl.dump(test_trajs, outfile)
-            
-            
-            # this is more for sanity checking
-            # with open('train_trajs.csv', 'w') as outfile:
-            #     for slice_2d in train_trajs:
-            #         np.savetxt(outfile, np.asarray(slice_2d), fmt='%d')
-            #         np.savetxt(outfile, np.asarray(["-------"]), fmt='%s')
-
-            # with open('final_train_trajs.csv', 'w') as outfile:
-            #     for slice_2d in final_train_trajs:
-            #         np.savetxt(outfile, np.asarray(slice_2d), fmt='%d')
-            #         np.savetxt(outfile, np.asarray(["-------"]), fmt='%s')
-            
-            # with open('val_start_states.csv', 'w') as outfile:
-            #     np.savetxt(outfile, np.asarray(val_perms), fmt='%d')
-                    
-            # with open('test_start_states.csv', 'w') as outfile:
-            #     np.savetxt(outfile, np.asarray(test_perms), fmt='%d')
-            
-        # Add each section as a separate trajectory in final train list
-    elif(compo_test_type == "strong"):
-        pass
-    
-    else:
-        raise NotImplementedError("You did not choose a compo test type that actually exists")
-        
-    
-    # TODO make trajectories noisy (Maybe)
-
-    return final_train_trajs, val_trajs, test_trajs
+    return train_trajs, val_trajs, test_trajs
 
 
 def one_step_mse(dlk_regressor, val_trajs):
-    mse_list = []
-    for i in range(len(val_trajs)):
-        true_val = val_trajs[i]
-        pred_val = dlk_regressor.simulate(true_val[0][np.newaxis, :], n_steps = 1)
-        mse = mean_squared_error(pred_val[1], true_val[1])
-        mse_list.append(mse)
-    
-    avg_mse = sum(mse_list)/float(len(mse_list))
-    return avg_mse, mse_list
+    val_trajs = np.stack(val_trajs)
+    pred_val = dlk_regressor.predict(val_trajs[:,0], n = 1)
 
+    mse = np.mean((pred_val - val_trajs[:,1])**2, axis=1)
+
+    return np.mean(mse), mse.tolist()
+
+
+def level_1_strong_gen_mse(dlk_regressor, val_trajs):
+    val_trajs = np.stack(val_trajs)
+
+    pred_val = dlk_regressor.predict(val_trajs[:,0], n = 1)
+
+    mse = (pred_val - val_trajs[:,1])**2
+
+    in_comb_mse_mask = np.ones_like(mse, dtype=np.int32)
+    in_comb_mse_mask[:,1] = 0
+    in_comb_mse_mask[:,0] = 0
+    in_comb_mse_mask[:,2] = 0
+    in_comb_mse_mask[:,3] = 0
+    in_comb_mse_mask[:,-1] = 0
+
+    out_comb_mse_mask = np.ones_like(mse,  dtype=np.int32)
+    out_comb_mse_mask[:,1] = 0
+    out_comb_mse_mask = 1 - in_comb_mse_mask
+
+    in_comb_mse = np.sum(in_comb_mse_mask * mse) / np.sum(in_comb_mse_mask)
+    out_comb_mse = np.sum(out_comb_mse_mask * mse) / np.sum(out_comb_mse_mask)
+
+    return in_comb_mse, out_comb_mse
 
 '''
             "relu": nn.ReLU(),
@@ -185,7 +177,7 @@ def one_step_mse(dlk_regressor, val_trajs):
             "linear": nn.Identity(),
 '''
 
-def train(train_data, val_data, look_forward, intrinsic_cords, hidden_dim, num_layers, batch_size=64, max_epochs=25, patience=5):
+def train(train_data, val_data, look_forward, intrinsic_cords, hidden_dim, num_layers, batch_size=64, max_epochs=25, patience=5, compo_test_type="weak"):
     state_size = train_data[0].shape[1]
     dlk_regressor = pk.regression.NNDMD(dt=1.0, look_forward=look_forward,
                                         config_encoder=dict(input_size=state_size,
@@ -208,11 +200,19 @@ def train(train_data, val_data, look_forward, intrinsic_cords, hidden_dim, num_l
         print(">>>>> On Epoch num : ", epoch)
         prev_mse_val = avg_mse_val
         dlk_regressor.fit(train_data)
+
         avg_mse_train, _ = one_step_mse(dlk_regressor, train_data)
         avg_mse_val, _ = one_step_mse(dlk_regressor, val_data)
+        
+
         print("Train avg 1 step MSE: ", avg_mse_train)
         print("Val avg 1 step MSE: ", avg_mse_val)
-        
+
+        if compo_test_type == 'strong':
+            in_comb_mse, out_comb_mse = level_1_strong_gen_mse(dlk_regressor, val_data)
+            print("Val avg 1 in combination MSE: ", in_comb_mse)
+            print("Val avg 1 out of combination MSE: ", out_comb_mse)
+            
         if(best_val > avg_mse_val):
             best_val = avg_mse_val
             best_regressor = deepcopy(dlk_regressor)
@@ -229,7 +229,6 @@ def train(train_data, val_data, look_forward, intrinsic_cords, hidden_dim, num_l
         if(inc > patience):
             print("Early stopping at epoch : ", epoch)
             break
-
             
     return best_regressor, best_val
 
@@ -237,36 +236,59 @@ def save_model(trained_koop_obj, model_save_dir):
     with open("{}/saved_best_model.pkl".format(model_save_dir), 'wb') as f:
         pkl.dump(trained_koop_obj, f)
 
-def run_experiment(params, exp_name):   
+def run_experiment(params, exp_name, seed):   
     results_dir = "./experiment_outputs/{}".format(exp_name)
     Path(results_dir).mkdir(parents=True, exist_ok=True) 
     
-    train_data, val_data, test_data = generate_data(state_size=params['state_size'],
-                                                    traj_len=params['traj_len'], rule=params['rule'],
-                                                    compo_test_type=params['compo_test_type'],
-                                                    results_dir=results_dir)
-        
+    if(params['compo_test_type'] == "weak"):
+        train_data, val_data, test_data = generate_data(state_size=params['state_size'],
+                                                        traj_len=params['traj_len'], rule=params['rule'],
+                                                        compo_test_type=params['compo_test_type'],
+                                                        results_dir=results_dir, seed=seed)
+    elif (params['compo_test_type'] == "strong"):
+        train_data, val_data, test_data = generate_data_for_strong_comp_raj(state_size=params['state_size'],
+                                                        rule=params['rule'],
+                                                        results_dir=results_dir,  seed=seed)
+    else:
+        raise NotImplementedError("You did not choose a compo test type that actually exists")
+    
     model, avg_mse = train(train_data, val_data, params['look_forward'], params['intrinsic_cords'], 
-          params['hidden_dim'], params['num_layers'])
+          params['hidden_dim'], params['num_layers'], compo_test_type=params['compo_test_type'])
     
     save_model(model, results_dir)
     
     test_avg_mse, test_mses = one_step_mse(model, test_data)
     print("Test avg mse: ", test_avg_mse)
-    print("Test MSEs: ", test_mses)
-    
-    np.save("{}/test_mse.npy".format(results_dir), np.array(test_mses))
-    plt.figure(figsize=(5, 10))
-    plt.boxplot(test_mses)
-    plt.ylim(0.0, 0.6)
-    plt.xticks([1], ["Rule {}".format(str(params['rule']))])
-    plt.savefig("{}/test_mse_plot.pdf".format(results_dir))
-    
+
+    if params['compo_test_type'] == 'strong':
+        in_comb_test_mse, out_comb_test_mse = level_1_strong_gen_mse(model, test_data)
+        print("Test avg 1 in combination MSE: ", in_comb_test_mse)
+        print("Test avg 1 out of combination MSE: ", out_comb_test_mse)
+        
+    stats_dict = dict()
+    stats_dict['test_avg_mse'] = test_avg_mse
+    stats_dict['in_comb_test_mse'] = in_comb_test_mse
+    stats_dict['out_comb_test_mse'] = out_comb_test_mse
+
+    out_file = open("{}/{}-test_mse.json".format(results_dir, seed), "w") 
+  
+    json.dump(stats_dict, out_file)
+
+    # np.save("{}/test_mse.npy".format(results_dir), np.array(test_mses))
+    # plt.figure(figsize=(5, 10))
+    # plt.boxplot(test_mses)
+    # plt.ylim(0.0, 0.6)
+    # plt.xticks([1], ["Rule {}".format(str(params['rule']))])
+    # plt.savefig("{}/test_mse_plot.pdf".format(results_dir))
     
 
 if __name__ in "__main__":
     exp_name = sys.argv[1]
     rule = int(sys.argv[2])
+    seed = int(sys.argv[3])
+
+    np.random.seed(seed) 
+
     
     params = {'state_size': 12,
             'traj_len': 50,
@@ -275,13 +297,10 @@ if __name__ in "__main__":
             'intrinsic_cords': 512,
             'hidden_dim': 128,
             'num_layers': 2,
-            'compo_test_type': "weak"
+            'compo_test_type': "strong"
             }
     
-    run_experiment(params, exp_name)
-    
-    
-    
+    run_experiment(params, exp_name, seed)
 
-    
-
+# changes to undo
+# 1) change max_epochs back to 1
